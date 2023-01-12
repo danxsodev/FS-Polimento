0550d2433c0d8e854aa3f9390f1d9562c1ee0770
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2023-01-12 00:48:08 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const buttonOpenModal = document.querySelector("#btn-open-modal");
+const buttonCloseModal = document.querySelector("#btn-close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+
+[buttonOpenModal, buttonCloseModal, fade].forEach((element) => {
+    element.addEventListener("click", () => toggleModal());
+});
+
+const yearCopyright = document.getElementById("year");
+yearCopyright.textContent = new Date().getFullYear();
+
