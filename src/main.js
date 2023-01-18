65fed0bcddcdacd55c148057f01dd35b54307183
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2023-01-18 01:53:39 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-const buttonOpenModal = document.querySelector("#btn-open-modal");
-const buttonCloseModal = document.querySelector("#btn-close-modal");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
+const buttonOpenModal = document.getElementById("btn-open-modal");
+const buttonCloseModal = document.getElementById("btn-close-modal");
+const modal = document.getElementById("modal");
+const fade = document.getElementById("fade");
+
+let currentImageIndex = 0;
+let images = document.querySelectorAll(".slider-container img");
+
+setInterval(function() {
+    images[currentImageIndex].style.opacity=0;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.opacity=1;
+}, 5000);
 
 const toggleModal = () => {
     modal.classList.toggle("hide");
