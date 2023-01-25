a7e7ad3a49ca692b9f80cdc8a5e80f3b8d062a77
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2023-01-25 20:22:03 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -24,10 +24,19 @@ function openMenu(event) {
     
     if (menuExpanded) {
         event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+        buttonBurger.innerHTML = `<svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 6L6 18" stroke="#EDEDF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M6 6L18 18" stroke="#EDEDF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`;
     } 
     
     else {
         event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+        buttonBurger.innerHTML = `<svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 12H21" stroke="#EDEDF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M3 6H21" stroke="#EDEDF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M3 18H21" stroke="#EDEDF7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>`;
     }
 }  
 buttonBurger.addEventListener("click", openMenu);
